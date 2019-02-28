ef9d5c9873cdfead0385b2363c1b34ea782d4fce
Soe Win Han <hans@berea.edu>
2019-02-27 21:37:42 -0500

    Update A04_cryptarithm.py

--- CSC 440/A04_cryptarithm.py
+++ CSC 440/A04_cryptarithm.py
@@ -7,23 +7,18 @@
 """
 Note: May take a long time depending on input and processing speed.
 """
-
-from itertools import chain
 from itertools import permutations
 
 
 def cryptarithm(first, second, sum):
-    alpha, temp, digits = [], '', '0123456789'
-    a, b, c = len(first), len(second), len(sum)
-    for i in chain(first, second, sum):
-        if i not in alpha:
-            alpha.append(i)
-    for i in permutations(digits, len(alpha)):
-        for j in chain(first, second, sum):
-            temp += i[alpha.index(j)]
+    abc, temp, a, b, c = list(first + second + sum), '', len(first), len(second), len(sum)
+    abc = list(dict.fromkeys(abc))
+    for i in permutations('0123456789', len(abc)):
+        for j in (first + second + sum):
+            temp += i[abc.index(j)]
         if int(temp[:a]) + int(temp[a:a+b]) == int(temp[a+b:]):
             if temp[:a][0] != '0' and temp[a:a+b][0] != '0' and temp[a+b:][0] != '0':
-                print(str(alpha) + "\n" + str(i) + "\n" + temp[:a] + " + " + temp[a:a + b] + " = " + temp[a + b:])
+                print(str(abc) + "\n" + str(i) + "\n" + temp[:a] + " + " + temp[a:a + b] + " = " + temp[a + b:])
         temp = ''
 
 
