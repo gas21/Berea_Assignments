fed30055f79bc73df8453cb0b20d2eccf7bd740f
Soe Win Han <hans@berea.edu>
2019-04-05 12:38:06 -0400

    much shorter

--- CSC 440/8 Queen Solver.py
+++ CSC 440/8 Queen Solver.py
@@ -1,6 +1,7 @@
 # Description: 8 Queen Solver. The program generates all 92 solutions for the 8 Queen Problem.
 # Author: Soe Win Han
 # Attribution: None
+#####################################################################
 
 from itertools import permutations
 
@@ -9,10 +10,8 @@ solutions = []
 boards = [i for i in permutations('01234567', 8)]
 for i in boards:
     stack = [(k, int(i[k])) for k in range(8)]
-    if all([all((all([(s[0] + l, s[1] + l) not in stack for l in range(1, 8)])) for s in stack),
-           all((all([(s[0] - l, s[1] + l) not in stack for l in range(1, 8)])) for s in stack),
-            all((all([(s[0] - l, s[1] - l) not in stack for l in range(1, 8)])) for s in stack),
-            all((all([(s[0] + l, s[1] + l) not in stack for l in range(1, 8)])) for s in stack)]):
+    if all([True if k not in stack else False for k in [m for s in stack for l in range(1, 8) for m in [(s[0] + l, s[1] + l),
+                (s[0] - l, s[1] + l), (s[0] - l, s[1] - l), (s[0] + l, s[1] + l)]]]):
         solutions.append(i)
 print(solutions)
 print(len(solutions))
---
+++ CSC 440/test.py
@@ -0,0 +1,4 @@
+s = [i for i in range(4) for j in range(3)]
+a = [True if i in s else False for i in range(5) for j in range(3)]
+print(s)
+print(a)
